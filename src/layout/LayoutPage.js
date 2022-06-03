be44c202ead36fa1eab8b6e89b96e5cb5813413b
truongdt <truongdt@relipasoft.com>
2022-06-03 09:13:34 +0700

    base layout

---
+++ src/layout/LayoutPage.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import { Layout, Menu } from "antd";
+const { Header, Content } = Layout;
+
+const LayoutPage = () => {
+  return (
+    <Layout>
+      <Header
+        style={{
+          position: "fixed",
+          zIndex: 1,
+          width: "100%",
+        }}
+      >
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key='1'>
+            <NavLink to="/">ButtonPage</NavLink>
+          </Menu.Item>
+          <Menu.Item key='2'>
+            <NavLink to="nav">NavbarPage</NavLink>
+          </Menu.Item>
+          <Menu.Item key='3'>
+            <NavLink to="header">HeaderPage</NavLink>
+          </Menu.Item>
+          <Menu.Item key='3'>
+            <NavLink to="modals">ModalPage</NavLink>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content
+        className="site-layout"
+        style={{
+          padding: "0 50px",
+          marginTop: 64,
+        }}
+      >
+        <div
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            minHeight: 380,
+          }}
+        >
+          <Outlet />
+        </div>
+      </Content>
+    </Layout>
+  );
+};
+
+export default LayoutPage;
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LayoutPage from './layout/LayoutPage'
+import ButtonPage from './page/ButtonPage'
+import HeaderPage from './page/HeaderPage'
+import ModalPage from './page/ModalPage'
+import NavbarPage from './page/NavbarPage'
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LayoutPage />}>
+        <Route index element={<ButtonPage />} />
+        <Route path="nav" element={<NavbarPage />} />
+        <Route path="header" element={<HeaderPage />} />
+        <Route path="modals" element={<ModalPage />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default Router
