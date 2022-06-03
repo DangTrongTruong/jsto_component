5526557cf2d2576d61568d8451d9f8d056d63504
truongdt <truongdt@relipasoft.com>
2022-06-03 15:58:17 +0700

    component header 009

--- src/component/header_009/Header_009.js
+++ src/component/header_009/Header_009.js
@@ -1,58 +1,38 @@
 import React from "react";
-// import icont1 from '../../assets/img/page/009/icon1.svg'
-// import icont2 from '../../assets/img/page/009/icon2.svg'
-// import icont3 from '../../assets/img/page/009/icon3.svg'
-// import icont4 from '../../assets/img/page/009/icon4.svg'
-// import icont5 from '../../assets/img/page/009/icon5.svg'
-// import icont6 from '../../assets/img/page/009/icon6.svg'
-// import icont7 from '../../assets/img/page/009/icon7.svg'
+import './Header_009.scss'
 
 const Header_009 = () => {
   return (
-    <>
+    <div className="wrapper-009_00_00">
       <div className="row select-title mt-3">
         <div className="col-md-12 d-flex align-items-center">
           <a className="text-decoration-none ms-4" href="/009_00_01.html">
             <div className="d-flex align-items-center">
-              <img src="/assets/img/page/009/icon1.svg" alt="" />
+              <img src="/assets/img/page/009/icont1.svg" alt="" />
               <span className="ms-2">ステータス一覧</span>
             </div>
           </a>
           <a className="text-decoration-none ms-4" href="/009_01_00.html">
             <div className="d-flex align-items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="18"
-                viewbox="0 0 27.238 21.894"
-              >
-                <path
-                  id="iconTrue"
-                  d="M-1597.672,318.223c-.2.166-.418.316-.6.5q-7.9,7.9-15.793,15.8c-.172.172-.327.362-.432.479l-9.2-9.188,3.362-3.334,5.535,5.664,13.554-13.6c1.065,1.1,2.011,2.08,2.968,3.05a5.784,5.784,0,0,0,.609.475Z"
-                  transform="translate(1624.41 -313.838)"
-                  fill="#9a9a9a"
-                  stroke="#2e2d2d"
-                  stroke-width="1"
-                ></path>
-              </svg>
+              <img src="/assets/img/page/009/icont2.svg" alt="" />
               <span className="ms-2 colorText--797979">入金済み一覧</span>
             </div>
           </a>
           <a className="text-decoration-none ms-4" href="/009_02_00.html">
             <div className="d-flex align-items-center">
-              <img src="/assets/img/page/009/icon2.svg" alt="" />
+              <img src="/assets/img/page/009/icont3.svg" alt="" />
               <span className="ms-2 colorText--797979">前払利用履歴</span>
             </div>
           </a>
           <a className="text-decoration-none ms-4" href="/009_03_00.html">
             <div className="d-flex align-items-center">
-              <img src="/assets/img/page/009/icon3.svg" alt="" />
+              <img src="/assets/img/page/009/icont4.svg" alt="" />
               <span className="ms-2 colorText--797979">支払確認一覧</span>
             </div>
           </a>
           <a className="text-decoration-none ms-4" href="/009_04_00.html">
             <div className="d-flex align-items-center select-title">
-              <img src="/assets/img/page/009/icon4.svg" alt="" />
+              <img src="/assets/img/page/009/icont5.svg" alt="" />
               <span className="ms-2 colorText--797979">マッチング状況</span>
             </div>
           </a>
@@ -67,7 +47,7 @@ const Header_009 = () => {
             </div>
             <input className="time inputCalender" type="input" />
             <div className="subfix">
-              <img src="/assets/img/page/009/icon5.svg" alt="" />
+              <img src="/assets/img/page/009/icont6.svg" alt="" />
             </div>
           </div>
           <span className="icon-spacing">〜</span>
@@ -77,14 +57,14 @@ const Header_009 = () => {
             </div>
             <input className="time inputCalender" type="input" />
             <div className="subfix">
-              <img src="/assets/img/page/009/icon6.svg" alt="" />
+              <img src="/assets/img/page/009/icont7.svg" alt="" />
             </div>
           </div>
           <a
             className="btn btn--32A5F7 ms-3 width-130 d-flex align-items-center justify-content-center"
             href="#"
           >
-            <img src="/assets/img/page/009/icon7.svg" alt="" />
+            <img src="/assets/img/page/009/icont8.svg" alt="" />
             <span className="ms-2">期間指定</span>
           </a>
         </div>
@@ -97,7 +77,7 @@ const Header_009 = () => {
               className="btn btn--494A52 d-flex align-items-center justify-content-center me-3"
               href="#"
             >
-              <span>支払い済み処理</span>
+              <span className="text-nowrap">支払い済み処理</span>
             </a>
             <a
               className="btn btn--404040 d-flex align-items-center justify-content-center"
@@ -108,7 +88,7 @@ const Header_009 = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/page/FormPage.js
+++ src/page/FormPage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import FormGroup1 from "../component/inputGroup/FormGroup1/FormGroup1";
-import icontLeftSrc from "../assets/img/page/002_03_01/icont2.svg";
-import icontRightSrc from "../assets/img/page/002_03_01/icont3.svg";
 
 const dataDesignForm = {
-  icontLeftSrc: icontLeftSrc,
+  icontLeftSrc: "/assets/img/page/002_03_01/icont2.svg",
   textLable: "トークンのアイコン（画像）",
   textPlaceholder: "画像ファイルを選んでください。",
   starError: "✴︎",
-  icontRightSrc: icontRightSrc,
+  icontRightSrc:"/assets/img/page/002_03_01/icont3.svg",
 };
 
 const FormPage = () => {
